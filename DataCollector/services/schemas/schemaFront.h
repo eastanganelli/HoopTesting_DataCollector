6b52df54e553d54f6c61897a3208a17abfaf2673
Ezequiel A. Stanganelli <ezequielstanganelli@gmail.com>
2024-06-12 01:16:51 -0300

    light changes

---
+++ DataCollector/services/schemas/schemaFront.h
@@ -0,0 +1,114 @@
+#ifndef SCHEMAFRONT_H
+#define SCHEMAFRONT_H
+#include "schemas.h"
+
+#define datetime_format "yyyy/MM/dd hh:mm:ss"
+
+namespace FrontClases {
+class NodeSetting {
+    const uint id;
+    uint times, temperature;
+public:
+    NodeSetting(const uint id_, const uint times_, const uint temperature_);
+    static QList<QSharedPointer<FrontClases::NodeSetting>> get(Schemas::Static& myDB, const uint idSpecification);
+
+    uint getID() const;
+    uint getTimes() const;
+    uint getTemperature() const;
+};
+
+class NodeSpecification {
+    const uint id;
+    QString specification,
+        description;
+public:
+    NodeSpecification(const uint id_, const QString specification_, const QString description_);
+    static QList<QSharedPointer<FrontClases::NodeSpecification>> get(Schemas::Static& myDB, const uint idMaterial);
+
+    uint getID() const;
+    QString getSpecification() const;
+};
+
+class NodeMaterial {
+    const uint id;
+    QString material, description, type;
+public:
+    NodeMaterial(const uint id_, const QString material_, const QString description_, const QString type_);
+    static QList<QSharedPointer<FrontClases::NodeMaterial>> get(Schemas::Static& myDB, const uint idStandard);
+
+    uint getID() const;
+    QString getMaterial() const;
+    QString getType() const;
+};
+
+class NodeEnviroment {
+    const uint id;
+    QString insertFluid, outFluid;
+public:
+    NodeEnviroment(const uint id_, const QString insertFluid_, const QString outFluid_);
+    static QList<QSharedPointer<FrontClases::NodeEnviroment>> get(Schemas::Static& myDB, const uint idStandard);
+
+    uint getID() const;
+    QString getEnviroment() const;
+};
+
+class NodeTestType {
+    const uint id;
+    QString testType;
+public:
+    NodeTestType(const uint id_, const QString testType_);
+    static QList<QSharedPointer<FrontClases::NodeTestType>> get(Schemas::Static& myDB, const uint idStandard);
+
+    uint getID() const;
+    QString getTestType() const;
+};
+
+class NodeConditionalPeriod {
+    const uint id;
+    uint minWall, maxWall;
+    QString conditionalPeriod;
+public:
+    NodeConditionalPeriod(const uint id_, const uint minwall_, const uint maxwall_, const QString condPeriod_);
+    static QList<QSharedPointer<FrontClases::NodeConditionalPeriod>> get(Schemas::Static& myDB, const uint idStandard);
+
+    uint getID() const;
+    uint getMinWall() const;
+    uint getMaxWall() const;
+    QString getConditionalPeriod() const;
+};
+
+class NodeEndCap {
+    const uint id;
+    QString endCap;
+public:
+    NodeEndCap(const uint id_, const QString endCap_);
+    static QList<QSharedPointer<FrontClases::NodeEndCap>> get(Schemas::Static& myDB, const uint idStandard);
+
+    uint getID() const;
+    QString getEndCap() const;
+};
+
+class NodeStandard {
+    const uint id;
+    QString standard;
+public:
+    NodeStandard(const uint id_, const QString standard_);
+    static QList<QSharedPointer<FrontClases::NodeStandard>> get(Schemas::Static &myDB);
+
+    uint getID() const;
+    QString getStandard() const;
+};
+
+class NodeOperator {
+    const uint id;
+    QString name, familyName;
+public:
+    NodeOperator(const uint id_, const QString name_, const QString familyName_);
+    static QList<QSharedPointer<FrontClases::NodeOperator>> get(Schemas::Static& myDB);
+
+    uint getID() const;
+    QString getFullName() const;
+};
+};
+
+#endif // SCHEMAFRONT_H
---
+++ DataCollector/services/schemas/schemas.cpp
@@ -0,0 +1,104 @@
+#include "defines.h"
+#include "services/database.h"
+#include <QSqlError>
+#include "schemas.h"
+
+Schemas::Data::Data(QLabel *dbstatus, QAction *dbconnect) {
+    this->lblConnectDB = dbconnect;
+    this->lblConnectionStatus = dbstatus;
+    this->myDB = QSqlDatabase::addDatabase("QMYSQL", DATA_SCHEMA_NAME);
+    this->myDB.setHostName("");
+    this->myDB.setPort(0);
+    this->myDB.setUserName("");
+    this->myDB.setPassword("");
+    this->myDB.setDatabaseName(DATA_DB_NAME);
+
+    try {
+        this->loadConfiguration();
+    }
+    catch(...) {}
+}
+
+Schemas::Data::~Data() { this->close(); }
+
+void Schemas::Data::changingLblConnectionState(const QString state_, const QString color_) {
+    this->lblConnectionStatus->setText("Base de Datos: " + state_);
+    this->lblConnectionStatus->setStyleSheet("color:" + color_ +";");
+}
+
+void Schemas::Data::loadConfiguration() {
+    DatabaseManager::loadConfiguration(this->myDB);
+}
+
+void Schemas::Data::open() {
+    try {
+        this->loadConfiguration();
+        this->prevState = !this->myDB.open();
+        qDebug() << this->myDB.lastError().text();
+        if(!this->prevState) { this->lblConnectDB->setText("Desconectar"); }
+    }
+    catch(DatabaseError::ConfigurationError* ex) {
+        QMessageBox myMsgBox(QMessageBox::Warning, "Configuración", ex->what());
+        myMsgBox.exec();
+    }
+}
+
+void Schemas::Data::close() {
+    if(!this->myDB.isOpen()) { return; }
+    this->myDB.close();
+    QSqlDatabase::removeDatabase(DATA_SCHEMA_NAME);
+    this->lblConnectDB->setText("Conectar");
+}
+
+bool Schemas::Data::isOpen() { return this->myDB.isOpen(); }
+
+bool Schemas::Data::status() {
+    bool state = false;
+    if(this->lblConnectionStatus != nullptr) {
+        state = this->myDB.isOpen();
+        if(!state && this->prevState) {
+#if CONSOLEDEBUGMODE == ConsoleConsoleDebugOn
+            qDebug() << "Base de datos: Desconectado";
+#endif
+            this->changingLblConnectionState("Desconectado", StatusRed);
+            this->prevState = false;
+        } else if (state && !this->prevState) {
+#if CONSOLEDEBUGMODE == ConsoleConsoleDebugOn
+            qDebug() << "Base de datos: Conectado";
+#endif
+            this->changingLblConnectionState("Conectado", StatusGreen);
+            this->prevState = true;
+        }
+    }
+    return state;
+}
+
+Schemas::Static::Static() { this->myDB.setDatabaseName("static_db"); }
+
+Schemas::Static::~Static() { this->close(); }
+
+bool Schemas::Static::open() {
+    bool isOpen = false;
+    try {
+        this->myDB = QSqlDatabase::addDatabase("QMYSQL", STATIC_SCHEMA_NAME);
+        this->myDB.setDatabaseName(STATIC_DB_NAME);
+        DatabaseManager::loadConfiguration(this->myDB);
+        isOpen = this->myDB.open();
+    }
+    catch(...) {
+        // QMessageBox myMsgBox(QMessageBox::Warning, "Configuración", ex->what());
+        // myMsgBox.exec();
+    }
+    return isOpen;
+}
+
+void Schemas::Static::close() {
+    if(!this->myDB.isOpen()) { return; }
+    this->myDB.close();
+    QSqlDatabase::removeDatabase(STATIC_SCHEMA_NAME);
+}
+
+QSqlQuery Schemas::Static::exec(QString query) {
+    QSqlQuery myQuery(query, this->myDB);
+    return myQuery;
+}
